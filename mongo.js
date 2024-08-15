5903e13ff93db9089b27d072a1dbcd0d9ed43fe9
Pyae Sone <pyaesonephyo@aceplussolutions.com>
2024-08-15 10:58:52 +0630

    exercise 3.19

---
+++ mongo.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+	console.log("give password as argument");
+	process.exit(1);
+}
+
+const password = process.argv[2];
+const name = process.argv[3];
+const number = process.argv[4];
+
+const url = `mongodb+srv://pyaesone:${password}@cluster0.y42f8.mongodb.net/phoneApp?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+	name: String,
+	number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (name && number) {
+	const entry = new Person({ name, number });
+
+	entry.save().then(() => {
+		console.log(`added ${name} number ${number} to phonebook`);
+		mongoose.connection.close();
+	});
+} else {
+	Person.find({}).then((result) => {
+		console.log("phonebook:");
+		result.forEach((entry) => {
+			console.log(`${entry.name} ${entry.number}`);
+		});
+		mongoose.connection.close();
+	});
+}
